db788357c37bbc3bbc5f21c6100e3474a67b7c14
ladykate17 <stamm.katherine@gmail.com>
2015-07-16 16:58:30 -0600

    dropdown info added

--- main.js
+++ main.js
@@ -1,47 +1,72 @@
-var projectBlockHtml = $('#project-block-template').html();
+var projectBlockHtml = $('#project-block-temp').html();
 var projectBlockTemplate = Handlebars.compile(projectBlockHtml);  
 
-var taskLineHtml = $('#task-line-template').html();
+var taskLineHtml = $('#task-line-temp').html();
 var taskLineTemplate = Handlebars.compile(taskLineHtml);  
 
+var taskInputHtml = $('#task-input-temp').html();
+var taskInputTemplate = Handlebars.compile(taskInputHtml);
 
-var shopLineHtml = $('#shop-line-template').html();
-var shopLineTemplate = Handlebars.compile(shopLineHtml);
+var shopBlockHtml = $('#list-block-temp').html();
+var shopBlockTemplate = Handlebars.compile(shopBlockHtml);
+
+var shopItemHtml = $('#item-line-temp').html();
+var shopItemTemplate = Handlebars.compile(shopItemHtml);
+
+var shopInputHtml = $('#item-input-temp').html();
+var shopInputTemplate = Handlebars.compile(shopInputHtml);
+
 
 
 $(document).on('ready', function() {
 	
 	var AllProjects= [];
 
-	var Project = function(name, priority, budget){
+	var Project = function(name, priority, priorityID, budget){
 		this.name 		= name;
 		this.priority 	= priority;
-		this.budget 	= budget;
+		this.priorityID	= priorityID || 4;
+		this.budget 	= budget || 0;
 		this.tasks 		= [];
 		AllProjects.push(this);
-	}
+	};
 
 	Project.prototype.create = function(){
-		var $projectBlock = projectBlockHtml;
+		var $projectBlock = projectBlockTemplate();
 		
 		this.el = $projectBlock;
 
 		return this.el
-	}
+	};
 
 
 
 	var Task = function(description){
 		this.description = description;
-	}
+	};
 
-	Task.prototype.create = function(){
-		var $taskBlock = taskLineHtml;
+	// Task.prototype.create = function(){
+	// 	var $taskBlock = taskLineTemplate();
 		
-		this.el = $taskBlock
+	// 	this.el = $taskBlock
 
-		return this.el
+	// 	return this.el
+	// };
+
+	var AllShopLists = [];
+
+	var ShopList = function(store){
+		this.store = store;
+		this.listItems = [];
+		AllShopLists.push(this);
+
 	}
+
+	var ListItem = function(description){
+		this.description = description;
+	};
+
+
 
 
 
@@ -61,7 +86,7 @@ $(document).on('ready', function() {
 
 	$(document).on('click', '.hide-form', function(){
 		$(this).parent().slideUp('slow')
-	})
+	});
 
 	$(document).on('click', '.add-project-btn.btn', function(){
 		$('.navi').slideDown('slow');
@@ -75,18 +100,18 @@ $(document).on('ready', function() {
 
 	var counter = 1; // this starts at 1 because index 0 is preloaded
 
-	$('.add-task').on('click', function(){
+	$('.add-task').on('click', function(){ // add another task input 
 		counter++
 		// console.log('counter ', counter);
-		var taskInput = '<div class="form-group"><div class="col-sm-offset-2 col-xs-9"><input data-id="' + counter + '" type="text" class="form-control proj-task" placeholder="Task (i.e. Paint, Buy Furniture, Hang Décor, etc.)"></div><div class="col-xs-1"><i class="fa fa-times-circle btn hide-input"></i></div></div>';
+		var taskInput = taskInputTemplate();
 
 		$(this).closest('.form-group').before(taskInput);
 
-	})
+	});
 
 	$(document).on('click', '.hide-input', function(){
 		$(this).closest('.form-group').slideUp('slow')
-	})
+	});
 
 	// SUBMIT PROJECT
 	$('.submit-project').on('click', function(){
@@ -98,67 +123,125 @@ $(document).on('ready', function() {
 		var budget 		= $('.proj-budget').val();
 		var description	= [this.tasks];
 
+		// $('form')[0].reset(); // clear/reset inputs
+			if (priority === "Select One") {
+				priority = "No Priority Set";
+				var priorityID = 0;
+			};
+
+			if (priority === "Low Priority") {
+				var priorityID = 3;
+			};
+
+			if (priority === "Medium Priority") {
+				var priorityID = 2;
+			};
+
+			if (priority === "High Priority") {
+				var priorityID = 1;
+			};
+
+		// console.log(budget);
+
 		// new project instance
-		var setProject 	= new Project(name, priority, budget);
+		var setProject 	= new Project(name, priority, priorityID, budget);
 
 		var tasks = $( ".proj-task" ).each(function( index ) { // loop over 'task' inputs to grab val() and push to array
 			var setTask = new Task( $( this ).val() );
 			setProject.tasks.push(setTask);
 			console.log(setTask.description);
 		});
-
-		$('input').val(''); // clear/reset inputs
-		// add some sort of logic here to remove "added" task inputs 
-		$('.main').last().append(projectBlockTemplate(setProject));
-		$('.tasks').last().append(taskLineTemplate(this.tasks));
-
-
-		// $('.main').last().append(setProject.create());
-		// $('.proj-title').append(name);
-		// $('.project').last().append('<p class="proj-sub-info"><strong>Priority Level:</strong> ' + priority + ' <strong>Budget for this Project:</strong> $' + budget + '.00</p><div class="row"><div class="col-xs-12"><h3 class="task-title">Tasks:</h3></div></div><div class="row tasks"></div>');
-		// $('.task-line').append(tasks.create());
-		// console.log('project: ', name, '\npriority: ', priority, '\nbudget: ', budget, '\ntasks: ', '??????')
-
 		console.log(AllProjects);
-	});
 
-	// var displayTasks = function(){
-	// 	$('.proj-task').each(function( index ){
-	// 		result = $('.project').append(this.tasks)
-	// 		return result
 
-	// 	});
-	// }
+
+		$('#project-form')[0].reset();
+		$('.btn-group button').removeClass('active');
+		$('div').remove('.addl-task-input')
+		$('.dropdown-menu').val(0);
+		// add some sort of logic here to remove "added" task inputs 
+		$('.projects-containter').append(projectBlockTemplate(setProject));
+		$('.tasks').last().append(taskLineTemplate(setProject));
+		$('.items-block').hide()
+
+		// SORT BY PRIORITY
+		_.indexBy(AllProjects, 'priorityID');
+		// var sortProjects = $( ".project-block" ).each(function( index ) { 
+		// 	var result = AllProjects.sort(function(a, b) { // sort the object in order of priority - high to low
+		// 		return b.priorityID - a.priorityID
+		// 		console.log(AllProjects);
+		// 	});
+		// });
+
+
+	}); // End Project Submit Click Event
+
 
 	$(document).on('click', '.collapse-caret', function(){
- 
- 		$(this).removeClass('fa-chevron-circle-left').addClass('fa-chevron-circle-down');
-		$(this).siblings('.project').slideDown('.tasks') 
+		if ( $(this).find('.fa-chevron-circle-left').hasClass('rotated') === true ){
+			$(this).find('.fa-chevron-circle-left').removeClass('rotated');
+			$(this).siblings('.text-block').find('.items-block').toggle()
+		}
+		else {
+	 		$(this).find('.fa-chevron-circle-left').addClass('rotated');
+	 		$(this).siblings('.text-block').find('.items-block').toggle()
+			
+		}
+
+
  	});
 
 	// --- Shopping List --- //
 	$('.shop-list-btn').on('click', function(){
 		// console.log('clicked')
-		$('.list').slideDown('slow');
+		$('.new-list-form').slideDown('slow');
+		$('.new-project-form').slideUp('slow');
+		$('.projects-containter').slideUp('slow');
 	});
 
-	$('.edit-item').on('click', function(){
-		$(this).replaceWith('<input type="text" class="form-control" value="' + $(this).text() + '"/><input type="submit" style="display:none"/>');
-	});
+	// I may come back to this if time
+	// $('.edit-item').on('click', function(){
+	// 	$(this).replaceWith('<input type="text" class="form-control" value="' + $(this).text() + '"/><input type="submit" style="display:none"/>');
+	// });
 
-	$(document).on('click', '.add-list-item', function(){
+	$(document).on('click', '.add-list-item', function(){ // add additional input
 		console.log('click');
-		var listInput = '<div class="form-group"><div class="col-xs-offset-1 col-xs-9"><input type="text" class="form-control shop-list-item" placeholder="New list item"></div><div class="col-xs-1"><button class="btn btn-sm save-item">save</button></div></div>';
-
+		var listInput = shopInputTemplate();
 		$(this).closest('.form-group').before(listInput);
+	});
 
-	})
+	$('.submit-list').on('click', function(){
+		event.preventDefault();
 
-	$(document).on('click', '.save-item', function(){
-		$('input').val('');
-		var listItem = $('.shop-list-item').val();
-		console.log(listItem)
-		$('.list-items').last().append(shopLineTemplate(listItem));
+		var store = $('.store-name').val();
+
+		var setList = new ShopList(store);
+
+		var listItems = $( ".list-item" ).each(function( index ) { // loop over 'task' inputs to grab val() and push to array
+			var setListItem = new ListItem( $( this ).val() );
+			setList.listItems.push(setListItem);
+			// console.log(setListItem.description);
+		});
+		console.log(AllShopLists);
+
+				// var setItem = "";
+
+				// var listItems = $( ".store-item" ).each(function( index ) { // loop over 'task' inputs to grab val() and push to array
+				// 	var newlistItem = new ShopList( $( this ).val() );
+				// 	setItem+= shopItemTemplate(newlistItem);
+				// 	console.log(newlistItem);
+				// 	setList.listItems.push(newlistItem);
+				// });
+		// var newlistItem = $('.shop-list-item').val();
+		// $('.main').last().append(shopBlockTemplate(---));
+		// $('.list').last().append(shopLineTemplate(---));
+
+		$('#list-form')[0].reset();
+		$('.btn-group button').removeClass('active');
+		$('.lists-containter').append(shopBlockTemplate(setList));
+		$('.lists').append(shopItemTemplate(setList));
+		$('.items-block').hide()
+
 	});
 
 
